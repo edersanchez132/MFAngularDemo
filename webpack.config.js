2bcdcbaadea834470ce796736d4f3825b76a6201
Eder Sanchez <emsanchez@elektra.com.mx>
2023-02-14 12:16:23 -0600

    Primer version

--- webpack.config.js
+++ webpack.config.js
@@ -28,24 +28,20 @@ module.exports = {
     new ModuleFederationPlugin({
         library: { type: "module" },
 
-        // For remotes (please adjust)
-        // name: "mfDemo",
-        // filename: "remoteEntry.js",
-        // exposes: {
-        //     './Component': './/src/app/app.component.ts',
-        // },        
+         name: "mfDemo",
+         filename: "remoteEntry.js",
+         exposes: {
+             './MostrardatosModule': './src/app/mostrardatos/mostrardatos.module.ts',
+         },        
         
-        // For hosts (please adjust)
-        // remotes: {
-        //     "mfe1": "http://localhost:3000/remoteEntry.js",
-
-        // },
+      
 
         shared: share({
           "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
           "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
           "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
           "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+  
 
           ...sharedMappings.getDescriptors()
         })
